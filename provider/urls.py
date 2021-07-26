9c9f06a95a068527dbbd153ce9b67c79142d4926
MsrdMai <sirilada.mai@gmail.com>
2021-07-26 20:47:40 +0700

    Repair type in Dep not show v1

--- provider/urls.py
+++ provider/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('department', views.department, name='department'),
     path('department/create_dep', views.create_dep, name='create_dep'),
-    path('department/<int:id>/type_in_dep/', views.type_in_dep, name='type_in_dep'),
+    path('department/<int:id>/type_in_dep', views.type_in_dep, name='type_in_dep'),
     path('department/<int:id>/type_in_dep/create_type_in_dep/', views.create_type_in_dep, name='create_type_in_dep'),
     path('type_in_dep/<int:id>/view_que/', views.view_que, name='view_que'),
     path('type_in_dep/<int:id>/forms/', views.forms, name='forms'),
